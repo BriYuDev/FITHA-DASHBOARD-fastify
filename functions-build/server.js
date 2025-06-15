e1b1c9ad79b77137b9621b779038004df849a4b1
BriYuDev <aesblox12@gmail.com>
2025-06-15 07:45:39 +0700

    v0.17

--- functions-build/server.js
+++ functions-build/server.js
@@ -3,4 +3,4 @@ import path from 'path'
 const __dirname = import.meta.dirname;
 const srcPath = path.dirname(__dirname)
 
-export { handler } from path.join(srcPath, '/lambda.js')
+export { handler } from path.join(srcPath, '/src/lambda.js')
